f8fb1d97fac7f0b59692f2efa415ba12082717e9
mehriiiii <mnmnmnm165k@gmail.com>
2026-09-15 13:32:12 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,14 @@ plugins {
 android {
     namespace = "com.example.calcpro"
     compileSdk = 35
+compileOptions {
+    sourceCompatibility JavaVersion.VERSION_17
+    targetCompatibility JavaVersion.VERSION_17
+}
 
+kotlinOptions {
+    jvmTarget = "17"
+}
     defaultConfig {
         applicationId = "com.example.calcpro"
         minSdk = 23
