7b88c609a7362865c37eb7a29834b39027ea2048
mehriiiii <mnmnmnm165k@gmail.com>
2026-09-15 15:14:30 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,18 +12,32 @@ android {
         applicationId = "com.example.calcpro"
         minSdk = 23
         targetSdk = 35
+
         versionCode = 2
         versionName = "1.1"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
     }
 
     signingConfigs {
         create("release") {
             val propsFile = rootProject.file("keystore.properties")
+
             if (propsFile.exists()) {
                 val props = java.util.Properties().apply {
                     propsFile.inputStream().use { load(it) }
                 }
-                storeFile = rootProject.file(props.getProperty("storeFile"))
+
+                storeFile = rootProject.file(
+                    props.getProperty("storeFile")
+                )
                 storePassword = props.getProperty("storePassword")
                 keyAlias = props.getProperty("keyAlias")
                 keyPassword = props.getProperty("keyPassword")
@@ -38,15 +52,20 @@ android {
         }
     }
 
-    buildFeatures { compose = true }
+    buildFeatures {
+        compose = true
+    }
 
     packaging {
-        resources { excludes += "/META-INF/{AL2.0,LGPL2.1}" }
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
     }
 }
 
 dependencies {
     val composeBom = platform("androidx.compose:compose-bom:2024.12.01")
+
     implementation(composeBom)
     androidTestImplementation(composeBom)
 
