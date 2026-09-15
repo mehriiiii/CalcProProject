ee422673001363b8850599941e7cfa6ca7dbccc3
mehriiiii <mnmnmnm165k@gmail.com>
2026-09-15 14:12:57 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,10 +7,12 @@ plugins {
 android {
     namespace = "com.example.calcpro"
     compileSdk = 35
-compileOptions {
-    sourceCompatibility JavaVersion.VERSION_17
-    targetCompatibility JavaVersion.VERSION_17
-}
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
     defaultConfig {
         applicationId = "com.example.calcpro"
         minSdk = 23
@@ -19,24 +21,28 @@ compileOptions {
         versionName = "1.1"
     }
 
-    buildFeatures { compose = true }
+    buildFeatures {
+        compose = true
+    }
 
     packaging {
-        resources { excludes += "/META-INF/{AL2.0,LGPL2.1}" }
+        resources {
+            excludes += "/META-INF/{AL2.0, LGPL2.1}"
+        }
     }
-}
 
-dependencies {
-    val composeBom = platform("androidx.compose:compose-bom:2024.12.01")
-    implementation(composeBom)
-    androidTestImplementation(composeBom)
+    dependencies {
+        val composeBom = platform("androidx.compose:compose-bom:2024.12.01")
+        implementation(composeBom)
+        androidTestImplementation(composeBom)
 
-    implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.activity:activity-compose:1.10.0")
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3:1.3.1")
-    implementation("androidx.compose.material:material-icons-extended")
+        implementation("androidx.core:core-ktx:1.15.0")
+        implementation("androidx.activity:activity-compose:1.10.0")
+        implementation("androidx.compose.ui:ui")
+        implementation("androidx.compose.ui:ui-tooling-preview")
+        implementation("androidx.compose.material3:material3:1.3.1")
+        implementation("androidx.compose.material:material-icons-extended")
 
-    debugImplementation("androidx.compose.ui:ui-tooling")
+        debugImplementation("androidx.compose.ui:ui-tooling")
+    }
 }
